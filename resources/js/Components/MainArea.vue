273e3b51df771d8548eaa42042ef9a4df391e266
Christopher Wray <chris@solmediaco.com>
2021-08-13 12:59:50 -0700

    create main area component

---
+++ resources/js/Components/MainArea.vue
@@ -0,0 +1,18 @@
+<template>
+  <main
+    :class="[
+      dontShowOnMobile ? 'hidden lg:block' : '',
+      'flex-1 relative z-0 overflow-y-auto focus:outline-none md:order-last',
+    ]"
+  >
+    <div class="py-6 sm:px-6 lg:px-8">
+      <slot></slot>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  props: { dontShowOnMobile: Boolean },
+};
+</script>
