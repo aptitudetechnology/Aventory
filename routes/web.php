27e3bb93e3f34e443f1add2496d865133d197f09
Christopher Wray <chris@solmediaco.com>
2021-08-24 17:06:36 -0700

    update products routes to use right middleware + plural

--- routes/web.php
+++ routes/web.php
@@ -52,13 +52,12 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::post('/vendors/archived/{vendorId}', [ArchivedVendorsController::class, 'store'])->name('archived-vendors.restore');
 
     Route::resource('vendors', App\Http\Controllers\VendorController::class);
+
+    Route::resource('products', App\Http\Controllers\ProductController::class);
+
+    Route::resource('features', App\Http\Controllers\FeatureController::class);
+
+    Route::resource('categories', App\Http\Controllers\CategoryController::class);
+
+    Route::resource('plants', App\Http\Controllers\PlantController::class);
 });
-
-
-Route::resource('product', App\Http\Controllers\ProductController::class);
-
-Route::resource('feature', App\Http\Controllers\FeatureController::class);
-
-Route::resource('category', App\Http\Controllers\CategoryController::class);
-
-Route::resource('plant', App\Http\Controllers\PlantController::class);
