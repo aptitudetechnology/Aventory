7a8ef42f225bac725447ce73f5c4c84379306867
Christopher Wray <chris@solmediaco.com>
2021-08-24 17:04:52 -0700

    add left padding to sidebar dropdown panel

--- resources/js/Components/SidebarDropdown.vue
+++ resources/js/Components/SidebarDropdown.vue
@@ -18,7 +18,7 @@
         <path d="M6 6L14 10L6 14V6Z" fill="currentColor" />
       </svg>
     </DisclosureButton>
-    <DisclosurePanel class="space-y-1">
+    <DisclosurePanel class="space-y-1 pl-4">
       <slot></slot>
     </DisclosurePanel>
   </Disclosure>
