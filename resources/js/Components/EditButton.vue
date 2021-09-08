a267423d212a5fe64030bb9ec1e3fc60498048b4
Christopher Wray <chris@solmediaco.com>
2021-09-08 08:35:08 -0700

    create editbutton vue component

---
+++ resources/js/Components/EditButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button class="w-4 h-4">
+    <EditIcon />
+  </button>
+</template>
+
+<script>
+import EditIcon from "@heroicons/vue/outline/PencilIcon";
+export default {
+  components: {
+    EditIcon,
+  },
+};
+</script>
