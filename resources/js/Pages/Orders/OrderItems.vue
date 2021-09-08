3b137a273c0be94349b7c6ff113932b51aa165e5
Christopher Wray <chris@solmediaco.com>
2021-09-08 08:38:59 -0700

    add order items to order show view

--- resources/js/Pages/Orders/OrderItems.vue
+++ resources/js/Pages/Orders/OrderItems.vue
@@ -1,1 +1,53 @@
-<template></template>
+<template>
+  <jet-action-section>
+    <template #title>Order Items</template>
+    <template #aside> <create-order-item :order="order" /> </template>
+    <template #content>
+      <div
+        class="
+          grid
+          gap-4
+          grid-cols-9
+          md:grid-cols-11
+          py-3
+          text-left text-xs
+          font-medium
+          text-gray-500
+          uppercase
+          tracking-wider
+        "
+      >
+        <div></div>
+        <div class="col-span-4 truncate">Product Name</div>
+        <div class="truncate">Size</div>
+
+        <div class="truncate">Confirmed</div>
+        <div class="truncate hidden md:block">Price</div>
+        <div class="truncate">Received</div>
+        <div class="truncate hidden md:block">Printed</div>
+        <div></div>
+      </div>
+      <div class="divide-gray-50 divide-y">
+        <order-line-item
+          v-for="item in orderItems"
+          :key="item.id"
+          :item="item"
+        />
+      </div>
+    </template>
+  </jet-action-section>
+</template>
+
+<script>
+import JetActionSection from "@/Jetstream/ActionSection.vue";
+import CreateOrderItem from "./CreateOrderItem.vue";
+import OrderLineItem from "./OrderLineItem.vue";
+export default {
+  props: { order: Object, orderItems: Array },
+  components: {
+    JetActionSection,
+    CreateOrderItem,
+    OrderLineItem,
+  },
+};
+</script>
--- resources/js/Pages/Orders/Show.vue
+++ resources/js/Pages/Orders/Show.vue
@@ -3,6 +3,7 @@
     <div>
       <div class="space-y-6">
         <update-order-form :order="order" />
+        <order-items :order="order" :orderItems="orderItems" />
         <delete-order-form :order="order" />
       </div>
     </div>
@@ -14,15 +15,18 @@ import OrdersLayout from "./OrdersLayout.vue";
 import UpdateOrderForm from "@/Pages/Orders/UpdateOrderForm";
 import ButtonLink from "@/Components/ButtonLink";
 import DeleteOrderForm from "./DeleteOrderForm.vue";
+import OrderItems from "./OrderItems.vue";
 export default {
   components: {
     OrdersLayout,
     UpdateOrderForm,
     ButtonLink,
     DeleteOrderForm,
+    OrderItems,
   },
   props: {
     order: Object,
+    orderItems: Array,
   },
 };
 </script>
