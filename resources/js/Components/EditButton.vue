c1eb7e7542b27329f1c68fed02d232a3b17557e2
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2021-09-08 16:30:27 -0700

    update edit button and add delete button

--- resources/js/Components/EditButton.vue
+++ resources/js/Components/EditButton.vue
@@ -1,14 +1,15 @@
 <template>
-  <button class="w-4 h-4">
-    <EditIcon />
-  </button>
+    <button class="hover:opacity-50 transition p-2">
+        <EditIcon :class="('stroke-current', ['w-' + size, 'h-' + size])" />
+    </button>
 </template>
 
 <script>
 import EditIcon from "@heroicons/vue/outline/PencilIcon";
 export default {
-  components: {
-    EditIcon,
-  },
+    props: { size: { default: 4 } },
+    components: {
+        EditIcon,
+    },
 };
 </script>
