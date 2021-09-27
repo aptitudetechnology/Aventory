1548dda06062f90a0d39482c0601017667d238bb
Christopher Wray <chris@solmediaco.com>
2021-09-27 12:54:45 -0700

    update localStorage to store sizeData in one property

--- resources/js/Pages/Inventory/Components/InventorySizeUpdate.vue
+++ resources/js/Pages/Inventory/Components/InventorySizeUpdate.vue
@@ -20,7 +20,7 @@
     <div class="flex items-center space-x-2">
       <jet-label class="text-lg">Change to: </jet-label>
       <JetButton
-        v-for="recent in recentSizes"
+        v-for="recent in sizeData.recentSizes"
         :key="recent.id"
         :disabled="this.selectedSize.id == recent.id"
         @click="selectedSize = recent"
@@ -31,14 +31,14 @@
     <div class="flex items-center space-x-2">
       <JetLabel for="autosize" class="text-lg">{{ autosizeLabel }}</JetLabel>
 
-      <JetCheckbox :checked="autosize" v-model="autosize" />
+      <JetCheckbox :checked="sizeData.autosize" v-model="sizeData.autosize" />
     </div>
     <div class="flex items-center space-x-2">
       <JetButton
-        v-for="recent in recentSizes"
+        v-for="recent in sizeData.recentSizes"
         :key="recent.id + 'recent'"
-        @click="autosizeSize = recent"
-        :disabled="autosizeSize?.id == recent.id"
+        @click="sizeData.autosizeSize = recent"
+        :disabled="sizeData.autosizeSize?.id == recent.id"
         >{{ recent.name }}</JetButton
       >
     </div>
@@ -66,9 +66,12 @@ export default {
   },
   data() {
     return {
-      autosize: false,
-      autosizeSize: null,
-      recentSizes: [],
+      sizeData: {
+        autosize: false,
+        autosizeSize: null,
+        recentSizes: [],
+      },
+
       selectedSize: this.sizes.find(
         (size) => size.id == this.inventory.size_id
       ),
@@ -84,8 +87,8 @@ export default {
       }
     },
     autosizeLabel() {
-      if (this.autosizeSize) {
-        return "Autosize to " + this.autosizeSize.name;
+      if (this.sizeData.autosizeSize) {
+        return "Autosize to " + this.sizeData.autosizeSize.name;
       } else {
         return "Autosize";
       }
@@ -93,49 +96,31 @@ export default {
   },
   mounted() {
     // Get local storage for preserving state across pages.
-    if (localStorage.getItem("autosizeSize")) {
+    if (localStorage.getItem("sizeData")) {
       try {
-        this.autosizeSize = JSON.parse(localStorage.getItem("autosizeSize"));
+        this.sizeData = JSON.parse(localStorage.getItem("sizeData"));
       } catch (error) {
-        localStorage.removeItem("autosizeSize");
+        localStorage.removeItem("sizeData");
       }
-    }
-
-    if (localStorage.getItem("recentSizes")) {
-      try {
-        this.recentSizes = JSON.parse(localStorage.getItem("recentSizes"));
-      } catch (error) {
-        localStorage.removeItem("recentSizes");
-      }
-    }
-
-    if (localStorage.autosize) {
-      localStorage.autosize == "true"
-        ? (this.autosize = true)
-        : (this.autosize = false);
     }
   },
   watch: {
     //Set local storage state.
-    autosizeSize(size) {
-      localStorage.setItem("autosizeSize", JSON.stringify(size));
-    },
-    autosize(bool) {
-      localStorage.autosize = bool;
-    },
-    recentSizes: {
-      handler(sizes) {
-        localStorage.setItem("recentSizes", JSON.stringify(sizes));
+    sizeData: {
+      handler(data) {
+        localStorage.setItem("sizeData", JSON.stringify(data));
       },
       deep: true,
     },
 
     selectedSize(value) {
-      if (!this.recentSizes.map((item) => item.id).includes(value.id)) {
-        if (this.recentSizes.length >= 3) {
-          this.recentSizes.shift();
+      if (
+        !this.sizeData.recentSizes.map((item) => item.id).includes(value.id)
+      ) {
+        if (this.sizeData.recentSizes.length >= 3) {
+          this.sizeData.recentSizes.shift();
         }
-        this.recentSizes.push(value);
+        this.sizeData.recentSizes.push(value);
       }
       if (value) {
         this.form.size_id = value.id;
