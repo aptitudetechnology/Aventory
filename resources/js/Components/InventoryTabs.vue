48b2a0c483def87103e30222166f9dbdd8a16932
Christopher Wray <chris@solmediaco.com>
2021-10-08 16:39:00 -0700

    refactor tabs to include pill style

---
+++ resources/js/Components/InventoryTabs.vue
@@ -0,0 +1,27 @@
+<template>
+    <tab-container>
+        <tab-link
+            :href="route('inventory.index')"
+            :current="route().current('inventory.index')"
+            type="pill"
+            >Inventory</tab-link
+        >
+        <tab-link
+            :href="route('inventory.clear-locations')"
+            :current="route().current('inventory.clear-locations')"
+            type="pill"
+            >Clear Assigned Locations</tab-link
+        >
+    </tab-container>
+</template>
+
+<script>
+import TabContainer from "./TabContainer.vue";
+import TabLink from "./TabLink.vue";
+export default {
+    components: {
+        TabContainer,
+        TabLink,
+    },
+};
+</script>
--- resources/js/Pages/Inventory/Index.vue
+++ resources/js/Pages/Inventory/Index.vue
@@ -1,35 +1,30 @@
 <template>
-  <app-layout>
-    <div class="flex-1 relative z-0 flex overflow-hidden">
-      <main-area :dontShowOnMobile="false">
-        <tab-container>
-          <tab-link :href="route('inventory.index')" :current="route().current('inventory.index')">Inventory</tab-link>
-          <tab-link :href="route('inventory.clear-locations')" :current="route().current('inventory.clear-locations')">Clear Assigned Locations</tab-link>
-        </tab-container>
-        <card class="max-w-screen-sm">
-          <search-inventory />
-        </card>
-      </main-area>
-    </div>
-  </app-layout>
+    <app-layout>
+        <div class="flex-1 relative z-0 flex overflow-hidden">
+            <main-area :dontShowOnMobile="false">
+                <InventoryTabs />
+                <card class="max-w-screen-sm">
+                    <search-inventory />
+                </card>
+            </main-area>
+        </div>
+    </app-layout>
 </template>
 
 <script>
 import AppLayout from "@/Layouts/AppLayout";
 import MainArea from "@Components/MainArea.vue";
 import Card from "@Components/Card.vue";
-import TabContainer from "@Components/TabContainer.vue";
-import TabLink from "@Components/TabLink.vue"
+import InventoryTabs from "@Components/InventoryTabs.vue";
 import SearchInventory from "./Components/SearchInventory.vue";
 
 export default {
-  components: {
-    AppLayout,
-    MainArea,
-    Card,
-    TabContainer,
-    TabLink,
-    SearchInventory,
-  },
+    components: {
+        AppLayout,
+        MainArea,
+        Card,
+        InventoryTabs,
+        SearchInventory,
+    },
 };
 </script>
