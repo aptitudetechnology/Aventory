f245a1c45dcf51dfbe7a70bb1cb45b01b65d653c
Christopher Wray <chris@solmediaco.com>
2021-10-19 13:56:49 -0700

    small change

--- resources/js/Pages/View/Show.vue
+++ resources/js/Pages/View/Show.vue
@@ -2,19 +2,19 @@
     <view-layout>
         <details-section>
             <template #title>{{ product.name }}</template>
-            <div class="col-span-6 sm:col-span-2">
+            <dl v-if="product.description" class="col-span-6 sm:col-span-2">
                 <dt class="text-sm font-medium text-gray-500">Description</dt>
                 <dd class="mt-1 text-sm text-gray-900">
                     {{ product.description }}
                 </dd>
-            </div>
-            <div v-if="product.plant" class="col-span-6 sm:col-span-2">
+            </dl>
+            <dl v-if="product.plant" class="col-span-6 sm:col-span-2">
                 <dt class="text-sm font-medium text-gray-500">Size</dt>
                 <dd class="mt-1 text-sm text-gray-900">
                     Height: {{ product.plant.height }} Spread:
                     {{ product.plant.spread }}
                 </dd>
-            </div>
+            </dl>
         </details-section>
     </view-layout>
 </template>
