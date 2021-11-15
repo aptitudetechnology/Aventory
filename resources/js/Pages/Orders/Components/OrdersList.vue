12d6bb6cb0e7be60982c4fb5187d391868e1883b
Christopher Wray <chris@solmediaco.com>
2021-11-15 15:29:15 -0800

    refactor index page slightly

---
+++ resources/js/Pages/Orders/Components/OrdersList.vue
@@ -0,0 +1,131 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<template>
+    <div class="bg-white shadow overflow-hidden sm:rounded-md">
+        <ul role="list" class="divide-y divide-gray-200">
+            <li v-for="order in orders.data" :key="order.id">
+                <a href="#" class="block hover:bg-gray-50">
+                    <div class="px-4 py-4 sm:px-6">
+                        <div class="flex items-center justify-between">
+                            <p
+                                class="
+                                    text-sm
+                                    font-medium
+                                    text-indigo-600
+                                    truncate
+                                "
+                            >
+                                {{ order.customer.name }}
+                            </p>
+
+                            <div class="ml-2 flex-shrink-0 flex">
+                                <p
+                                    class="
+                                        px-2
+                                        inline-flex
+                                        text-xs
+                                        leading-5
+                                        font-semibold
+                                        rounded-full
+                                        bg-green-100
+                                        text-green-800
+                                    "
+                                >
+                                    {{ order.paymentStatus?.name }}
+                                </p>
+                            </div>
+                        </div>
+                        <div class="mt-2 sm:flex sm:justify-between">
+                            <div class="sm:flex">
+                                <p
+                                    class="
+                                        flex
+                                        items-center
+                                        text-sm text-gray-500
+                                    "
+                                >
+                                    #
+                                    {{ order.id }}
+                                </p>
+                                <div
+                                    class="
+                                        mt-2
+                                        flex
+                                        items-center
+                                        text-sm text-gray-500
+                                        sm:mt-0
+                                    "
+                                >
+                                    <CalendarIcon
+                                        class="
+                                            flex-shrink-0
+                                            mr-1.5
+                                            h-5
+                                            w-5
+                                            text-gray-400
+                                        "
+                                        aria-hidden="true"
+                                    />
+                                    <p>
+                                        <time :datetime="order.date">{{
+                                            order.date
+                                        }}</time>
+                                    </p>
+                                </div>
+                            </div>
+                            <div
+                                class="
+                                    mt-2
+                                    flex
+                                    items-center
+                                    text-sm text-gray-500
+                                    sm:mt-0
+                                "
+                            >
+                                <CalendarIcon
+                                    class="
+                                        flex-shrink-0
+                                        mr-1.5
+                                        h-5
+                                        w-5
+                                        text-gray-400
+                                    "
+                                    aria-hidden="true"
+                                />
+                                <p>
+                                    <time :datetime="order.date">{{
+                                        order.date
+                                    }}</time>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import {
+    CalendarIcon,
+    LocationMarkerIcon,
+    UsersIcon,
+} from "@heroicons/vue/solid";
+import moment from "moment";
+
+export default {
+    components: {
+        CalendarIcon,
+        LocationMarkerIcon,
+        UsersIcon,
+    },
+    props: {
+        orders: {
+            type: Object,
+        },
+    },
+    methods: {
+        formatDate: (value) => moment(value).format("MM/DD/YYYY"),
+    },
+};
+</script>
--- resources/js/Pages/Orders/Index.vue
+++ resources/js/Pages/Orders/Index.vue
@@ -30,128 +30,7 @@
                     </div>
                 </details-section>
             </div>
-            <details-section>
-                <template #title>Recent Orders</template>
-                <template #aside
-                    ><search-input v-model="search"></search-input
-                ></template>
-                <div class="col-span-6 overflow-auto">
-                    <table-table class="text-left">
-                        <table-head>
-                            <table-h
-                                ><div
-                                    class="
-                                        select-none
-                                        cursor-pointer
-                                        flex
-                                        items-center
-                                    "
-                                    :class="
-                                        orderBy == 'id' ? 'text-gray-900' : ''
-                                    "
-                                    @click="updateOrderBy('id')"
-                                >
-                                    Order #
-                                    <ArrowUpIcon
-                                        v-if="
-                                            orderBy == 'id' &&
-                                            orderByDirection == 'asc'
-                                        "
-                                        class="ml-2 w-3 h-3"
-                                    />
-                                    <ArrowDownIcon
-                                        v-else
-                                        class="ml-2 w-3 h-3"
-                                    /></div
-                            ></table-h>
-                            <table-h
-                                ><div
-                                    class="
-                                        select-none
-                                        cursor-pointer
-                                        flex
-                                        items-center
-                                    "
-                                    :class="
-                                        orderBy == 'date' ? 'text-gray-900' : ''
-                                    "
-                                    @click="updateOrderBy('date')"
-                                >
-                                    Date
-                                    <ArrowUpIcon
-                                        v-if="
-                                            orderBy == 'date' &&
-                                            orderByDirection == 'asc'
-                                        "
-                                        class="ml-2 w-3 h-3"
-                                    />
-                                    <ArrowDownIcon
-                                        v-else
-                                        class="ml-2 w-3 h-3"
-                                    /></div
-                            ></table-h>
-                            <table-h
-                                ><div
-                                    class="
-                                        select-none
-                                        cursor-pointer
-                                        flex
-                                        items-center
-                                    "
-                                    :class="
-                                        orderBy == 'customer'
-                                            ? 'text-gray-900'
-                                            : ''
-                                    "
-                                    @click="updateOrderBy('customer')"
-                                >
-                                    Customer
-                                    <ArrowUpIcon
-                                        v-if="
-                                            orderBy == 'customer' &&
-                                            orderByDirection == 'asc'
-                                        "
-                                        class="ml-2 w-3 h-3"
-                                    />
-                                    <ArrowDownIcon
-                                        v-else
-                                        class="ml-2 w-3 h-3"
-                                    />
-                                </div>
-                            </table-h>
-                            <table-h>Status</table-h>
-                            <table-h>Total</table-h>
-                        </table-head>
-                        <tbody>
-                            <tr
-                                v-for="order in orders.data"
-                                :key="order.id"
-                                tabindex="0"
-                                class="
-                                    px-2
-                                    rounded-md
-                                    hover:bg-gray-100
-                                    focus:bg-gray-100
-                                    transition
-                                    cursor-pointer
-                                "
-                                @click="
-                                    $inertia.get(route('orders.show', order))
-                                "
-                            >
-                                <table-d>{{ order.id }}</table-d>
-                                <table-d>{{ formatDate(order.date) }}</table-d>
-                                <table-d>{{ order.customer.name }}</table-d>
-                                <table-d>{{
-                                    order.deliveryStatus?.name
-                                }}</table-d>
-                                <table-d>{{ order.grand_total }}</table-d>
-                            </tr>
-                        </tbody>
-                    </table-table>
-                    <pagination :items="orders" />
-                </div>
-            </details-section>
+            <view-orders :orders="orders" :filters="filters" />
         </div>
     </orders-layout>
 </template>
@@ -167,14 +46,7 @@ import ButtonLink from "@/Components/ButtonLink";
 import DetailsSection from "@/Components/DetailsSection";
 import HeadingOne from "@Components/HeadingOne.vue";
 import CompletedOrdersCard from "./Components/CompletedOrdersCard.vue";
-import TableTable from "@/Components/Tables/TableTable.vue";
-import TableHead from "@/Components/Tables/TableHead.vue";
-import TableH from "@/Components/Tables/TableH.vue";
-import TableD from "@/Components/Tables/TableD.vue";
-import Pagination from "@/Components/Pagination.vue";
-import SearchInput from "@/Components/SearchInput.vue";
-import _debounce from "lodash/debounce";
-import moment from "moment";
+import ViewOrders from "./Components/ViewOrders.vue";
 export default {
     components: {
         ExternalLinkIcon,
@@ -185,12 +57,7 @@ export default {
         DetailsSection,
         HeadingOne,
         CompletedOrdersCard,
-        TableTable,
-        TableHead,
-        TableH,
-        TableD,
-        SearchInput,
-        Pagination,
+        ViewOrders,
     },
     props: {
         orders: {
@@ -198,45 +65,6 @@ export default {
         },
         filters: {
             type: Object,
-        },
-    },
-    data() {
-        return {
-            search: this.filters.search || "",
-            orderBy: this.filters.orderBy || "",
-            orderByDirection: this.filters.orderByDirection || "",
-        };
-    },
-    watch: {
-        search: _debounce(function (value) {
-            this.updateSearch();
-        }, 200),
-    },
-    methods: {
-        formatDate: (value) => moment(value).format("MM/DD/YYYY"),
-        updateOrderBy(value) {
-            if (this.orderBy === value) {
-                this.orderByDirection =
-                    this.orderByDirection === "desc" ? "asc" : "desc";
-            } else {
-                this.orderBy = value;
-                this.orderByDirection = "desc";
-            }
-            this.updateSearch();
-        },
-        updateSearch() {
-            this.$inertia.get(
-                this.route("orders.index"),
-                {
-                    search: this.search,
-                    orderBy: this.orderBy,
-                    orderByDirection: this.orderByDirection,
-                },
-                {
-                    preserveState: true,
-                    replace: true,
-                }
-            );
         },
     },
 };
