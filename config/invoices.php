3a189bdddcd2a031e26419b48531981e30ec200e
Christopher Wray <chris@solmediaco.com>
2022-01-04 20:43:30 -0800

    add Invoice package

---
+++ config/invoices.php
@@ -0,0 +1,97 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+
+return [
+    'date' => [
+        /*
+         * Carbon date format
+         */
+        'format' => 'm/d/Y',
+        /*
+         * Due date for payment since invoice's date.
+         */
+        'pay_until_days' => 7,
+    ],
+
+    'serial_number' => [
+        'series'   => 'AA',
+        'sequence' => 1,
+        /*
+         * Sequence will be padded accordingly, for ex. 00001
+         */
+        'sequence_padding' => 0,
+        'delimiter'        => '.',
+        /*
+         * Supported tags {SERIES}, {DELIMITER}, {SEQUENCE}
+         * Example: AA.00001
+         */
+        'format' => '{SEQUENCE}',
+    ],
+
+    'currency' => [
+        'code' => 'USD',
+        /*
+         * Usually cents
+         * Used when spelling out the amount and if your currency has decimals.
+         *
+         * Example: Amount in words: Eight hundred fifty thousand sixty-eight EUR and fifteen ct.
+         */
+        'fraction' => 'cents',
+        'symbol'   => '$',
+        /*
+         * Example: 19.00
+         */
+        'decimals' => 2,
+        /*
+         * Example: 1.99
+         */
+        'decimal_point' => '.',
+        /*
+         * By default empty.
+         * Example: 1,999.00
+         */
+        'thousands_separator' => ',',
+        /*
+         * Supported tags {VALUE}, {SYMBOL}, {CODE}
+         * Example: 1.99 €
+         */
+        'format' => '{SYMBOL} {VALUE}',
+    ],
+
+    'paper' => [
+        // A4 = 210 mm x 297 mm = 595 pt x 842 pt
+        'size'        => 'letter',
+        'orientation' => 'portrait',
+    ],
+
+    'disk' => 'local',
+
+    'seller' => [
+        /*
+         * Class used in templates via $invoice->seller
+         *
+         * Must implement LaravelDaily\Invoices\Contracts\PartyContract
+         *      or extend LaravelDaily\Invoices\Classes\Party
+         */
+        'class' => \LaravelDaily\Invoices\Classes\Seller::class,
+
+        /*
+         * Default attributes for Seller::class
+         */
+        'attributes' => [
+            'name'          => '',
+            'address'       => '',
+            'code'          => '',
+            'vat'           => '',
+            'phone'         => '',
+            'custom_fields' => [
+                /*
+                 * Custom attributes for Seller::class
+                 *
+                 * Used to display additional info on Seller section in invoice
+                 * attribute => value
+                 */],
+        ],
+    ],
+];
