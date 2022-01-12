bea9bf789eedcd1234078a556b5442ad3985fcb8
Christopher Wray <chris@solmediaco.com>
2022-01-12 15:58:08 -0800

    add base tests for quickbooks syncing feature.

---
+++ tests/Feature/LinkQuickBooksTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LinkQuickBooksTest extends TestCase
+{
+    /**
+     * Teams should be able to link to QuickBooks.
+     *
+     * @return void
+     */
+    public function test_teams_can_link_a_quickbooks_account()
+    {
+        $user = User::factory()->withPersonalTeam()->create();
+        $team = $user->currentTeam;
+
+        $response = $this->post('/teams/'.$team->id.'/link-quickbooks', [
+            'company_id' => $team->id
+            'access_token' => 'abcdef',
+            'realm_id' => 'ghijk',
+            'refresh_token' => 'lmnop',
+        ]);
+    }
+}
