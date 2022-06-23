f97b701547b20a0a374b5d4377ac8ad282bdd81d
Christopher Wray <chris@solmediaco.com>
2022-06-23 16:28:15 -0700

    add company meta data to teams table

---
+++ database/migrations/2022_06_23_160356_add_company_meta_data_to_teams_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('teams', function (Blueprint $table) {
+            $table->text('phone')->nullable();
+            $table->text('email')->nullable();
+            $table->text('address_line_1')->nullable();
+            $table->text('address_line_2')->nullable();
+            $table->text('city')->nullable();
+            $table->text('state')->nullable();
+            $table->text('zip')->nullable();
+            $table->text('country')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('teams', function (Blueprint $table) {
+            //
+        });
+    }
+};
