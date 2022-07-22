44dc62b2d06137974a5f280be1a931b98e421360
Chris Wray <chris@solmediaco.com>
2022-07-22 15:45:00 -0700

    fix relative logo issue

--- resources/js/Jetstream/AuthenticationCardLogo.vue
+++ resources/js/Jetstream/AuthenticationCardLogo.vue
@@ -1,6 +1,6 @@
 <template>
     <img
-        src="logos/awia-for-nurseries-zoomed.png"
+        src="/logos/awia-for-nurseries-zoomed.png"
         alt="Awia for Nurseries"
         class="h-20 mb-4 w-auto"
     />
