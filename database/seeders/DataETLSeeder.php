46ceb0303843531dc9685133bb448444bd6f1e89
dedicated <osefson@gmail.com>
2022-07-24 15:56:55 +0000

    Added ETL of Location and Product

--- database/seeders/DataETLSeeder.php
+++ database/seeders/DataETLSeeder.php
@@ -10,6 +10,11 @@ use Illuminate\Support\Facades\Artisan;
 use App\Models\Category;
 use App\Models\Size;
 use App\Models\Team;
+use App\Models\NurseryLocation;
+use App\Models\Product;
+use App\Models\Plant;
+use App\Models\Order;
+use App\Models\OrderItem;
 
 class DataETLSeeder extends Seeder
 {
@@ -18,38 +23,23 @@ class DataETLSeeder extends Seeder
      *
      * @return void
      */
+    public function __construct()
+    {
+        $this->team = NULL;
+        $this->sqlsrv_conn = DB::connection('sqlsrv');
+    }
+
     public function run()
     {
         $this->fresh_db();
 
-        $team = $this->create_team();
-        $sqlsrv_conn = DB::connection('sqlsrv');
+        // Create Garden Gate Nursery team
+        $this->team = $this->create_team();
 
-        // ETL of categories
-        $old_categories = $sqlsrv_conn->table('TblCategories')->get()->toArray();
-        $new_categories = array_map(function($category) use ($team) {
-            return [
-                'id' => $category->CategoryID,
-                'name' => $category->Category,
-                'team_id' => $team->id
-            ];
-        }, $old_categories);
-
-        Category::insert($new_categories);
-
-        // ETL of sizes
-        $old_sizes = $sqlsrv_conn->table('TblProductSizes')->get()->toArray();
-        $new_sizes = array_map(function($size) use ($team) {
-            return [
-                'id' => $size->SizeID,
-                'name' => $size->Description,
-                'next_size_id' => $size->NextSizeID,
-                'sort_num' => $size->SortID,
-                'team_id' => $team->id
-            ];
-        }, $old_sizes);
-
-        Size::insert($new_sizes);
+        $this->do_ETL_categories();
+        $this->do_ETL_sizes();
+        $this->do_ETL_locations();
+        $this->do_ETL_products();
     }
 
     public function fresh_db()
@@ -61,6 +51,84 @@ class DataETLSeeder extends Seeder
 
     public function create_team()
     {
-        return Team::factory()->create();
+        return Team::factory()->create([
+            'name' => 'Garden Gate Nursery'
+        ]);
+    }
+    
+    public function do_ETL_categories()
+    {
+        $old_categories = $this->sqlsrv_conn->table('TblCategories')->get()->toArray();
+        $new_categories = array_map(function($category) {
+            return [
+                'id' => $category->CategoryID,
+                'name' => $category->Category,
+                'team_id' => $this->team->id
+            ];
+        }, $old_categories);
+
+        Category::insert($new_categories);
+    }
+
+    public function do_ETL_sizes()
+    {
+        $old_sizes = $this->sqlsrv_conn->table('TblProductSizes')->get()->toArray();
+        $new_sizes = array_map(function($size) {
+            return [
+                'id' => $size->SizeID,
+                'name' => $size->Description,
+                'next_size_id' => $size->NextSizeID,
+                'sort_num' => $size->SortID,
+                'team_id' => $this->team->id
+            ];
+        }, $old_sizes);
+
+        Size::insert($new_sizes);
+    }
+
+    public function do_ETL_locations()
+    {
+        $old_locations = $this->sqlsrv_conn->table('TblLocations')->get()->toArray();
+        $new_locations = array_map(function($l) {
+            return [
+                'id' => $l->LocationID,
+                'name' => $l->LocationName,
+                'team_id' => $this->team->id
+            ];
+        }, $old_locations);
+
+        NurseryLocation::insert($new_locations);
+    }
+
+    public function do_ETL_products()
+    {
+        $old_products = $this->sqlsrv_conn->table('TblProducts')->get()->toArray();
+        $new_products = [];
+        $new_plants = [];
+        foreach ($old_products as $p) {
+            array_push($new_products, [
+                'id' => $p->ProductID,
+                'team_id' => $this->team->id,
+                'category_id' => $p->CategoryID,
+                'name' => $p->CommonName,
+                'type' => 'plant',
+                'description' => $p->Description,
+            ]);
+
+            array_push($new_plants, [
+                'product_id' => $p->ProductID,
+                'scientific_name' => $p->ScientificName,
+                'common_name' => $p->CommonName,
+                'zone' => $p->Zone,
+                'height' => $p->Height,
+                'spread' => $p->Spread,
+                'bloom_color' => $p->Bloom,
+                'fall_color' => $p->FallColor,
+                'growth_rate' => $p->GrowthRate,
+            ]);
+        }
+
+        Product::insert($new_products);
+        Plant::insert($new_plants);
     }
 }
