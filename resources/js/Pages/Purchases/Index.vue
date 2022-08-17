165c99758207e1ea7f65f555c98d7a0405e34372
Chris Wray <chris@solmediaco.com>
2022-08-16 17:57:48 -0700

    add button and heading to purchases index

--- resources/js/Pages/Purchases/Index.vue
+++ resources/js/Pages/Purchases/Index.vue
@@ -2,6 +2,8 @@
 import PurchasesLayout from "./PurchasesLayout.vue";
 import PurchasesView from "./Components/PurchasesView.vue";
 
+import HeadingOne from "@/Components/Headings/HeadingOne.vue";
+import ButtonLink from "@/Components/Links/ButtonLink.vue";
 defineProps({
     purchases: {
         type: Object,
@@ -14,6 +16,12 @@ defineProps({
 
 <template>
     <PurchasesLayout>
+        <div class="flex justify-between items-center w-full pb-9 px-2 lg:px-4">
+            <HeadingOne>Purchases</HeadingOne>
+            <ButtonLink :href="route('purchases.create')"
+                >New Purchase</ButtonLink
+            >
+        </div>
         <PurchasesView
             :purchases="purchases"
             :filters="filters"
