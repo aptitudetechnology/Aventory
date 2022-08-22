cf4f9931afbec0259e28a4e9aebc8dc2719a4368
Chris Wray <chris@solmediaco.com>
2022-08-22 16:29:48 -0700

    run build assets

--- resources/js/Components/Links/NavLink.vue
+++ resources/js/Components/Links/NavLink.vue
@@ -3,7 +3,7 @@
         :href="href"
         :class="[
             current ? 'bg-gray-50' : 'hover:bg-gray-50 hover:text-gray-900',
-            'text-gray-600 text-large group flex items-center px-2 py-2 text-base rounded-md',
+            'text-gray-700 text-large group flex items-center px-2 py-2 text-base rounded-md',
         ]"
     >
         <slot></slot>
