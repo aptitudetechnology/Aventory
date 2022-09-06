bb9e24ea01b1c408f5cabe92736b0a24f34c4152
Chris Wray <chris@solmediaco.com>
2022-09-06 14:40:49 -0700

    refactor navigation- click to go to main item

--- resources/js/Pages/Customers/CustomersLayout.vue
+++ resources/js/Pages/Customers/CustomersLayout.vue
@@ -1,19 +1,47 @@
+<script setup>
+import { computed } from "vue";
+import AppLayout from "@/Layouts/AppLayout.vue";
+import BackLink from "@/Components/Links/BackLink.vue";
+import CustomersAside from "@/Pages/Customers/CustomersAside.vue";
+import MainArea from "@/Components/MainArea.vue";
+import ButtonLinkVue from "@/Components/Links/ButtonLink.vue";
+const isIndex = computed(
+    () =>
+        route().current("customers.index") ||
+        route().current("customers.archived-index")
+);
+</script>
+
 <template>
     <app-layout>
         <div class="flex-1 relative z-0 flex overflow-hidden">
             <main-area :dontShowOnMobile="isIndex">
-                <nav v-if="!isIndex" aria-label="Breadcrumb" class="xl:hidden">
-                    <div class="px-4 md:pb-2">
-                        <back-link
-                            v-if="route().current('customers.*')"
-                            :href="route('customers.index')"
-                            >Customers</back-link
-                        >
-                        <back-link
-                            v-else
-                            :href="route('archived-customers.index')"
-                            >Archived Customers</back-link
-                        >
+                <nav aria-label="Breadcrumb">
+                    <div class="px-4 md:pb-2 flex justify-between">
+                        <div class="xl:hidden">
+                            <back-link
+                                v-if="route().current('customers.*')"
+                                :href="route('customers.index')"
+                                >Customers</back-link
+                            >
+                            <back-link
+                                v-else
+                                :href="route('archived-customers.index')"
+                                >Archived Customers</back-link
+                            >
+                        </div>
+
+                        <div class="ml-auto grid grid-cols-2 gap-4">
+                            <ButtonLinkVue
+                                class="btn-secondary"
+                                :href="route('customer-price-levels.index')"
+                            >
+                                Customer Price Levels
+                            </ButtonLinkVue>
+                            <ButtonLinkVue :href="route('customers.create')">
+                                New Customer
+                            </ButtonLinkVue>
+                        </div>
                     </div>
                 </nav>
                 <!-- Main Content -->
@@ -26,27 +54,3 @@
         </div>
     </app-layout>
 </template>
-
-<script>
-import AppLayout from "@/Layouts/AppLayout.vue";
-import BackLink from "@/Components/Links/BackLink.vue";
-import CustomersAside from "@/Pages/Customers/CustomersAside.vue";
-import MainArea from "@/Components/MainArea.vue";
-
-export default {
-    components: {
-        CustomersAside,
-        AppLayout,
-        BackLink,
-        MainArea,
-    },
-    computed: {
-        isIndex() {
-            return (
-                route().current("customers.index") ||
-                route().current("archived-customers.index")
-            );
-        },
-    },
-};
-</script>
